595f1a547e62aa986ddfca17d373ed25df4380a2
Laukik Landage <work.laukik@gmail.com>
2026-05-06 03:06:10 +0530

    Update: Use large colour-blue-lightbg logo and update git

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header" id="header">
-      <img src="/colour-Logo.png" alt="Lenodel Labs" className="header__logo-img" />
+      <img src="/colour-blue-lightbg.png" alt="Lenodel Labs" className="header__logo-img" />
     </header>
   );
 }
